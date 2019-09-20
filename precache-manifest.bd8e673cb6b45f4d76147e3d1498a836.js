370792c2d4acc6327c54f24041effd1f2299379f
Thanh Do <thanh_do@apple.com>
2019-09-20 14:04:16 -0700

    Updates

---
+++ precache-manifest.bd8e673cb6b45f4d76147e3d1498a836.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "526a029feeb8655e136aace5fee0b904",
+    "url": "/ms/index.html"
+  },
+  {
+    "revision": "2a0f1d2bec826c043f36",
+    "url": "/ms/static/css/main.43be1d98.chunk.css"
+  },
+  {
+    "revision": "8b485b8f2be0d2a536ee",
+    "url": "/ms/static/js/2.f9c0d3ec.chunk.js"
+  },
+  {
+    "revision": "2a0f1d2bec826c043f36",
+    "url": "/ms/static/js/main.5bb2ce4e.chunk.js"
+  },
+  {
+    "revision": "48d761fbf857db4c71eb",
+    "url": "/ms/static/js/runtime~main.933051a0.js"
+  },
+  {
+    "revision": "647259d512a712312787582638c86eb8",
+    "url": "/ms/static/media/bomb.647259d5.png"
+  }
+]);
